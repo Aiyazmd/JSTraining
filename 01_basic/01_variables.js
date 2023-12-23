cd3630f2f037c27f82360dd3cf5b5a40fea81c2c
Aiyazmd <70117146+Aiyazmd@users.noreply.github.com>
2023-12-23 14:09:47 +0000

    data types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+//What are variables in javascript:
+// JavaScript Variables are the building blocks of any programming language. In JavaScript, 
+// variables can be used to store reusable values.
+//  The values of the variables are allocated using the assignment operator(“=”).
+
+const accountId="123";
+let accountEmail="Aiyaz@gmail.com";
+var accountPassword="123456"
+let accountState;
+// accountId=2  here we cannot assign a new value to the variable which ia implemented with const keyword
+console.log(accountId)
+//console.table is used to pring more the one value in our console the we will make use of console.table
+console.table([accountId,accountEmail,accountPassword,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,33 @@
+//USe Strict 
+//Why do we use use strict
+// By using the use strict method then javascript treats all the code as newer version
+//Accorning to new standards it wont allow to use old standers code if we us it it will throw some king of erros
+
+
+
+
+
+
+//Data Types in javaacript :
+//Javascript is dynamically types (losely type) scripting language. 
+//In Javascript variables can recieve different data type over time.
+
+//The latest ecmascript defined 8 data types where as there are 7 premitive and 1 is non premetive data type:
+
+//Premitive:
+
+//Null
+//Boolen
+//String
+//Symbol
+//Number
+//Undefined
+//BigInt
+
+//Non-Premetive:
+
+//Object.
+
+
+//Null Data type:
+The Null Stores a 
